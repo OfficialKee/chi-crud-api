ecfa8d6ead5629e1aca1c3c5c34f4b3bc8fb1a6e
Kee Maloney <keemaloney@Kees-MBP.attlocal.net>
2024-06-10 21:45:45 -0400

    started routers

--- main.go
+++ main.go
@@ -10,12 +10,31 @@ func main() {
 	r := chi.NewRouter()
 // middleware logger for http reuquestsa
 	r.Use(middleware.Logger)
+
 	r.Get("/", func(w http.ResponseWriter, r *http.Request){
 		w.Write([]byte("OK"))
 	})
 
+	// using the created routes for the book resource all the routes are mounted to as new router which is then mounted to main router
+	r.Mount("/books", BookRoutes())
+	
+
 // server on port 3000
 	http.ListenAndServe(":3000", r)
 
-	// i
+
+}
+
+func BookRoutes() chi.Router {
+	r := chi.NewRouter()
+    
+	bookHandler := BookHandler{}
+
+	r.Get("/",bookHandler.ListBooks)
+	r.Post("/",bookHandler.CreateBook)
+	r.Get("/{id}", bookHandler.GetBook)
+	r.Put("/{id}", bookHandler.UpdateBook)
+	r.Delete("/{id}", bookHandler.DeleteBook)
+
+	return r
 }
